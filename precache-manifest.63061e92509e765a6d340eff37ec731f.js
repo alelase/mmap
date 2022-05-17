81295bbc98e3756073e114107011923a17b6827a
alejandrol <alejandrol@mprest.com>
2022-05-17 15:34:10 +0300

    Updates

---
+++ precache-manifest.63061e92509e765a6d340eff37ec731f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ff8bd82023bf7c662ece7d9e076320",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5159c39c616695ca025c",
+    "url": "./static/css/main.9e24c1c7.chunk.css"
+  },
+  {
+    "revision": "55e3348dac138f910a45",
+    "url": "./static/js/2.f233075f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f233075f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5159c39c616695ca025c",
+    "url": "./static/js/main.caf0a528.chunk.js"
+  },
+  {
+    "revision": "9b99b29e9c0d840f26b8",
+    "url": "./static/js/runtime-main.ff4632e8.js"
+  }
+]);
