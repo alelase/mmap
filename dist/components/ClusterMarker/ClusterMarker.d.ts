db4f065b50d4ed11eb50e23649f1dfd0220d0345
alejandrol <alejandrol@mprest.com>
2022-05-17 14:38:28 +0300

    push dist

--- dist/components/ClusterMarker/ClusterMarker.d.ts
+++ dist/components/ClusterMarker/ClusterMarker.d.ts
@@ -5,6 +5,7 @@ interface ClusterMarkerOptions extends google.maps.MarkerOptions {
     position: google.maps.LatLngLiteral;
     count: number;
     onClick: () => void;
+    onRightClick: () => void;
 }
 declare const ClusterMarker: React.FC<ClusterMarkerOptions>;
 export default ClusterMarker;
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -49,7 +49,7 @@ const Map: React.FC<MapProps> = ({
   // [START maps_react_map_component_event_hooks]
   useEffect(() => {
     if (map) {
-      ['click', 'idle', 'bounds_changed'].forEach((eventName) =>
+      ['click', 'rightclick', 'idle', 'bounds_changed'].forEach((eventName) =>
         google.maps.event.clearListeners(map, eventName)
       );
 
